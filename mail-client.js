e936b65d71b11866f8a42978f28b5f6b8b225b05
Simon Kaltgrad <simonkalt@gmail.com>
2021-06-14 09:57:20 -0700

    Testing sms from parsed email from netlivy

--- mail-client.js
+++ mail-client.js
@@ -10,3 +10,16 @@ const inbox = {
  tls: true,
  tlsOptions: { rejectUnauthorized: false }
 };
+
+notifier(inbox).on('mail', (mail) => {
+    const mailObject = {
+        from: mail.from[0].address,
+        name: mail.from[0].name,
+        date: mail.receivedDate,
+        subject: mail.subject ? mail.subject : 'No Subject',
+        message: mail.text && mail.text.replace(/\s/g,'').length > 0? mail.text : 'No Message body',
+        attachments: mail.attachments ? mail.attachments.map(value => value.fileName).join(' ') : 'None',
+    };
+    request.post(process.env.WEBHOOK_TARGET).form(mailObject);
+    console.info("New Email Received!")
+ }).start();
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('dotenv').config({path: './config.env'});
+require('dotenv').config({ path: './config.env' });
 
 const express = require('express');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const twilio_client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN); 
+const twilio_client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 const moment = require('moment');
 const nodemailer = require('nodemailer');
 
@@ -12,7 +12,7 @@ const nodemailer = require('nodemailer');
 const app = express();
 
 //Basic Express App Setup
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static(__dirname + '/client'));
 
 //Handlers
@@ -26,5 +26,54 @@ app.get('/', (req, res) => {
 
 app.post('/new-email', (req, res) => {
     console.log(req.body);
+    parseToSMS(req.body);
     res.sendStatus(200);
- });
+});
+
+function parseToSMS(emailObject) {
+    const maxChars = 1500; //Changed to 1500 because of Twilio's injection into free accounts - move it up to 1600 later'
+    const formattedDate = moment(emailObject.date).format('ddd, MMM Do YYYY, h:m A');
+    let messageBody = emailObject.message;
+
+    const smsBodyHeader = 'New email from:${emailObject.from}, at ${formattedDate}.\nSub: ${emailObject.subject}\nAttachments: ${emailObject.attachments}\n';
+    let sms = smsBodyHeader + messageBody;
+
+    //Helpers
+    let availableChars = maxChars - smsBodyHeader.length;
+    const continueMsg = "\nContinued in Next SMS. - Number ";
+
+    if (messageBody.length > availableChars) {
+        console.info("HUGE EMAIL DETECTED - splitting to save us.");
+        let counter = 1;
+        availableChars -= (continueMsg.length + 2); // + 2 for the SMS Number place holder
+        while (messageBody.length > 0) {
+            console.info("Sending remaining chunks of size:" + availableChars);
+            if (counter == 1) {
+                sms = smsBodyHeader + messageBody.substring(0, availableChars) + continueMsg + counter;
+                availableChars += smsBodyHeader.length // No More headers in subsequent SMSes..
+            }
+            else if (messageBody.length <= availableChars)
+                sms = messageBody;
+            else
+                sms = messageBody.substring(0, availableChars) + continueMsg + counter;
+            messageBody = messageBody.substring(availableChars, messageBody.length);
+            sendSMS(sms);
+            counter += 1;
+        }
+    } else {
+        sendSMS(sms);
+    }
+    console.info("Done Sending!.");
+}
+
+function sendSMS(smsbody) {
+    twilio_client.messages.create({
+        to: '+18184195986',  // change this to customer phone number
+        from: process.env.TWILIO_NUMBER,
+        body: smsbody,
+    },
+        (err, message) => {
+            if (err)
+                console.log("Error sending SMS:" + err);
+        });
+}
